22aa21943a003a29e5dda3307fa530794eccf23e
Andrew Luca <aluca@winify.com>
2018-09-07 11:32:45 +0300

    chore: move to angular

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import angular from 'angular'
+import 'angular-cookies'
+import registerServiceWorker from './registerServiceWorker'
+/* eslint-disable-next-line */
+import appTemplate from 'raw-loader!./my-app.html'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class MyController {
+    constructor($cookies) {
+        this.$cookies = $cookies
+    }
+    user = 'world';
+
+    onClick() {
+        this.$cookies.put('666', 666)
+        window.alert(666)
+    }
+}
+MyController.$inject = ['$cookies']
+
+angular
+    .module('app', ['ngCookies'])
+    .component('myApp', {
+        template: appTemplate,
+        controller: ['$cookies', class {
+            constructor($cookies) {
+                this.$cookies = $cookies
+            }
+            user = 'world';
+        
+            onClick() {
+                this.$cookies.put('666', 666)
+                window.alert(666)
+            }
+        }]
+    });
+
+angular.bootstrap(document, ['app']);
 registerServiceWorker();
